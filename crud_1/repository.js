1d928c2c148e04edbbba1051eb25badef7d977a7
Sokolov1111 <94213710+Sokolov1111@users.noreply.github.com>
2021-11-20 09:46:36 +0300

    Add files via upload

---
+++ crud_1/repository.js
@@ -0,0 +1,16 @@
+module.exports =  new function (){
+    const data = {};
+    let increment =0;
+    this.create = dt =>{
+        dt.Id = increment++;
+        data[dt.Id] = dt;
+        return dt;
+    }
+    this.getAll = () => Object.value(data);
+    this.get = id => data[id]
+    this.update = dt => {
+        data[dt.Id] = dt;
+        return dt;
+    }
+    this.delete = id => delete data[id]
+}
